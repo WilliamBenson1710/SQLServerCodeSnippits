5ee6af38122e1e600813d466d9ad5fb3bce56d20
WilliamBenson1710 <88503539+WilliamBenson1710@users.noreply.github.com>
2021-08-20 09:44:46 -0700

    Create CheckPatching.sql

---
+++ CheckPatching.sql
@@ -0,0 +1,22 @@
+SELECT 
+CAST(SERVERPROPERTY('ServerName') AS NVARCHAR(100)) AS [ServerName]
+,CAST(@@SERVICENAME AS NVARCHAR(100)) AS [ServiceName]
+,CONVERT(NVARCHAR(40), CASE LEFT(CONVERT(NVARCHAR(40), SERVERPROPERTY('ProductVersion')),4) 
+   WHEN '8.00' THEN 'SQL Server 2000'
+   WHEN '9.00' THEN 'SQL Server 2005'
+   WHEN '10.0' THEN 'SQL Server 2008'
+   WHEN '10.5' THEN 'SQL Server 2008 R2'
+   WHEN '11.0' THEN 'SQL Server 2012'
+   WHEN '12.0' THEN 'SQL Server 2014'
+   WHEN '13.0' THEN 'SQL Server 2016'
+   WHEN '14.0' THEN 'SQL Server 2017'
+   WHEN '15.0' THEN 'SQL Server 2019'
+   ELSE 'SQL Server 2019+'
+END) AS [SQLVersionBuild]
+,CAST(SERVERPROPERTY('Edition')AS NVARCHAR(100)) AS [Edition]
+,CAST(@@VERSION AS NVARCHAR(1000)) AS SQLVersion
+,LTRIM(RTRIM(SUBSTRING(@@Version, CHARINDEX('Windows',@@Version), CHARINDEX('<',@@Version) - CHARINDEX('Windows',@@Version)))) AS OSVersion
+,CAST(SERVERPROPERTY('EngineEdition') AS NVARCHAR(100)) AS [EngineEdition]             
+,CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(100)) AS [ProductVersion]
+,(SELECT COUNT(1) FROM master.dbo.sysprocesses WHERE program_name = N'SQLAgent - Generic Refresher') AS SQLServerAgentRunning
+,(SELECT sqlserver_start_time FROM sys.dm_os_sys_info) AS SQLServerStartTime
